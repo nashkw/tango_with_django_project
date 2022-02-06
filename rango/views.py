f06dbe832dc3cd62e17253c99cb0f1149864532b
Nash Waugh <2517003W@student.gla.ac.uk>
2022-02-06 09:17:15 +0000

    chapter 9 exercises completed

--- rango/views.py
+++ rango/views.py
@@ -36,7 +36,8 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
-    
+
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -49,6 +50,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -112,7 +114,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
     
 @login_required
 def user_logout(request):
